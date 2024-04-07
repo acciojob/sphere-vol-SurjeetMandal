8156821b19d4a3babd93fd4e17bc208b7092ec8a
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-07 14:03:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 function volume_sphere(radius) {
     //Write your code here
-	let pi = 22/7;
-	let fourThird = 4/3
-	let volume = fourThird * pi * r * r * r;
+	let r = radius;
+	let volume = (4/3) * Math.PI * r * r * r;
 	let roundVolume = volume.toFixed(4);
 	return roundVolume;
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
-
 let form = document.getElementById("MyForm")
 
-form.addEventListener("click",function(event) {
+form.addEventListener("submit",function(event) {
 	event.preventDefault();
 	
-	let radius = parseFloat(document.querySelector("#radius").value);
+	let radius = parseFloat(document.querySelector('#radius').value);
 	
 	if (!isNaN(radius)) {
 		let volume_of_sphere = volume_sphere(radius);
-		document.querySelector("#volume").value = volume_of_sphere;
+		document.querySelector('#volume').value = volume_of_sphere;
 	}
 	else{
 		alert('Please enter a valid number for the radius.');
